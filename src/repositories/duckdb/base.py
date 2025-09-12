97fb3aad9cd8d55fae9982e75700272f13fe24c1
leson207 <sonthaile2002@gmail.com>
2025-09-12 08:43:46 +0700

    delete duckdb exporter

--- src/repositories/duckdb/base.py
+++ src/repositories/duckdb/base.py
@@ -15,11 +15,9 @@ class BaseRepository:
     def __init__(
         self,
         sql_schema: Type,
-        python_schema: Type,
     ):
         self.db = session
         self.sql_schema = sql_schema
-        self.python_schema = python_schema
         self.table_name = sql_schema.__tablename__
         self.primary_keys = [col.name for col in sql_schema.__table__.primary_key]
 
