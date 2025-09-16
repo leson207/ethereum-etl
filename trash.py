da04997ebb77c5214e54193c2878fc3ddfd31a55
leson207 <sonthaile2002@gmail.com>
2025-09-16 22:19:44 +0700

    remove temp file

--- trash.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-if __name__ == "__main__":
-    from src.configs.nats_conn import nats_init
-    asyncio.run(nats_init())
-    from src.configs.nats_conn import jetstream
-    asyncio.run(jetstream.publish("ethereum.test", b"first"))
-    print("8"*100)
